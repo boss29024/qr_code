cd580a0f645af95a68415023f81577e8b330f5e4
boss29024 <boss29024@gmail.com>
2020-09-14 15:18:30 +0700

    -แก้

--- android/app/src/main/kotlin/com/example/qr_code/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/qr_code/MainActivity.kt
@@ -3,5 +3,4 @@ package com.example.qr_code
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    Toast.makeText(applicationContext, "Codemobiles",Toast.LENGTH).show()
 }
