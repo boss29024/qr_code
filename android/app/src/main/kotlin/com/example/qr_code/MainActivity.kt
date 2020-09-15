f25d9f895c14a2b6cb6de8a2b50e2006b6bb6a83
boss29024 <boss29024@gmail.com>
2020-09-15 11:23:40 +0700

    -คอร์

--- android/app/src/main/kotlin/com/example/qr_code/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/qr_code/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.widget.Toast
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
+    private val CHANNEL = "cm.share/share";
 
-        Toast.makeText(applicationContext, "Codemobiles", Toast.LENGTH_LONG).show()
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            methodCall, result ->
+            if(methodCall.method == "shareFile"){
+                Toast.makeText(applicationContext, "Codemobiles", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
     }
 }
 
