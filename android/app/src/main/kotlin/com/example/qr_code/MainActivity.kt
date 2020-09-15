8bf84ca9a76a3560d5425c571015b11ce2ae55cf
boss29024 <boss29024@gmail.com>
2020-09-15 11:55:27 +0700

    -androidManifest

--- android/app/src/main/kotlin/com/example/qr_code/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/qr_code/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.qr_code
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.FileProvider
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import java.io.File
 
 
 class MainActivity: FlutterActivity() {
@@ -17,10 +20,23 @@ class MainActivity: FlutterActivity() {
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             methodCall, result ->
             if(methodCall.method == "shareFile"){
-                Toast.makeText(applicationContext, "Codemobiles", Toast.LENGTH_LONG).show()
-
+                shareFile(methodCall.arguments as String);
             }
+        }
+    }
 
+    // add
+    private fun shareFile(path: String) {
+        val imageFile = File(cacheDir, path)
+        val contentUri = FileProvider.getUriForFile(this, "cm.qrcode.share", imageFile)
+        // cm.qrcode.share (ref. AndroidManifest.xml)
+
+        Intent(Intent.ACTION_SEND).let {
+            it.type = "image/png"
+            it.putExtra(Intent.EXTRA_STREAM, contentUri)
+            startActivity(Intent.createChooser(it, "Share QRCode"))
+
+            Toast.makeText(applicationContext, "Codemobiles", Toast.LENGTH_LONG).show()
         }
     }
 }
